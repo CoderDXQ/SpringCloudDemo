bc5b3a1a9f4881f82dd0577651b0ffa5d1669669
cococolabear <794055465@qq.com>
2021-01-06 23:55:12 +0800

    SpringBoot自动配置和包扫描

--- demo/src/main/java/cn/itcast/springboot/TestApplication.java
+++ demo/src/main/java/cn/itcast/springboot/TestApplication.java
@@ -1,9 +1,20 @@
 package cn.itcast.springboot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * @author Duan Xiangqing
  * @version 1.0
  * @date 2021/1/6 11:40 下午
  */
+//引导类 SpringBoot的入口
+@EnableAutoConfiguration //开启自动配置
+@ComponentScan  //开启包扫描 递归向下扫描该类所在的包
 public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
 }
--- demo/src/main/java/cn/itcast/springboot/controller/Hello2Controller.java
+++ demo/src/main/java/cn/itcast/springboot/controller/Hello2Controller.java
@@ -1,7 +1,5 @@
 package cn.itcast.springboot.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("hello")
+@RequestMapping("hello2")
 //@EnableAutoConfiguration //启动自动配置
-public class HelloController {
+public class Hello2Controller {
 
     @GetMapping("show")
     public String test() {
