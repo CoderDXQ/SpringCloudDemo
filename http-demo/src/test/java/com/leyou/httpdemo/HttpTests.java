e36a17fbb29fcee55c9198abe2b3623cd71c1e42
cococolabear <794055465@qq.com>
2021-01-07 14:01:16 +0800

    ObjectMapper的各种序列化与反序列化方法

--- http-demo/src/test/java/com/leyou/httpdemo/HttpTests.java
+++ http-demo/src/test/java/com/leyou/httpdemo/HttpTests.java
@@ -1,6 +1,7 @@
 package com.leyou.httpdemo;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.leyou.httpdemo.pojo.User;
 import org.apache.http.client.methods.HttpGet;
@@ -12,6 +13,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class HttpTests {
 
@@ -56,7 +59,7 @@ public class HttpTests {
         System.out.println(user);
     }
 
-    //    json处理工具
+    //    json处理工具 各种序列化与反序列化
 //    private ObjectMapper mapper = new ObjectMapper();
     @Test
     public void testJson() throws IOException {
@@ -67,11 +70,39 @@ public class HttpTests {
         user.setUserName("liuyan");
 //        打印类信息
         System.out.println(user);
-        // 序列化
+        System.out.println();
+
+        // 序列化 对象转json字符串
         String json = MAPPER.writeValueAsString(user);
-        System.out.println("json = " + json);
-//        反序列化
+        System.out.println("序列化json = " + json);
+        System.out.println();
+
+//        反序列化 json转普通对象 两个参数：json字符串和反序列化的目标类字节码
         User user1 = MAPPER.readValue(json, User.class);
-        System.out.println(user1);
+        System.out.println("反序列化：" + user1);
+        System.out.println();
+
+//        json转集合
+        user1.setName("gg");
+        User[] us = {user, user1};
+        String jsons = MAPPER.writeValueAsString(us);
+//        两个参数：json字符串和反序列化的目标类字节码
+        List<User> users = MAPPER.readValue(jsons, MAPPER.getTypeFactory().constructCollectionType(List.class, User.class));
+        for (User u : users) {
+            System.out.println(u);
+        }
+        System.out.println();
+
+//        json转换任意复杂类型
+        String jsonss = MAPPER.writeValueAsString(Arrays.asList(user, user));
+        List<User> users1 = MAPPER.readValue(jsonss, new TypeReference<List<User>>() {
+        });
+        System.out.println(users1);
+        for (User u : users1) {
+            System.out.println(u);
+        }
+        System.out.println();
+
+
     }
 }
