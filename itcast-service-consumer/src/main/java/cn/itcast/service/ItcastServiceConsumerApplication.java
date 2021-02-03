933302edc607d13a974b923a881f3eee1bb19efa
cococolabear <794055465@qq.com>
2021-02-04 03:26:03 +0800

    Hystrix服务降级

--- itcast-service-consumer/src/main/java/cn/itcast/service/ItcastServiceConsumerApplication.java
+++ itcast-service-consumer/src/main/java/cn/itcast/service/ItcastServiceConsumerApplication.java
@@ -2,6 +2,7 @@ package cn.itcast.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableDiscoveryClient//启动Eureka客户端
+@EnableCircuitBreaker//开启开启熔断器
 public class ItcastServiceConsumerApplication {
 
     @Bean
--- itcast-service-consumer/src/main/java/cn/itcast/service/controller/UserController.java
+++ itcast-service-consumer/src/main/java/cn/itcast/service/controller/UserController.java
@@ -2,9 +2,12 @@ package cn.itcast.service.controller;
 
 import cn.itcast.service.pojo.User;
 //import com.netflix.discovery.DiscoveryClient;
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,9 +34,33 @@ public class UserController {
     private DiscoveryClient discoveryClient;//包含了拉取的所有微服务地址
 
     //消费者访问地址 http://localhost:8080/consumer/user?id=28
+
+
+    /**
+     * //    http://localhost:8080/consumer/user?id=28 可以使用这个地址
+     *
+     * @GetMapping
+     * @ResponseBody //    @HystrixCommand(fallbackMethod = "queryUserByIdFallback") //调用失败触发熔断机制
+     * public User queryUserById(@RequestParam("id") Long id) {
+     * //从Eureka获取服务实例
+     * //                List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
+     * //                ServiceInstance instance = instances.get(0);
+     * <p>
+     * //        填写生产者的url地址 id用28  硬编码地址
+     * //        return this.restTemplate.getForObject("http://localhost:8081/user/" + id, User.class);
+     * <p>
+     * //解决硬编码地址问题
+     * //                return this.restTemplate.getForObject("http://" + instance.getHost() + ":" + instance.getPort() + "/user/" + id, User.class);
+     * <p>
+     * //        使用ribbon负载均衡的写法  地址是生产者的地址
+     * return this.restTemplate.getForObject("http://service-provider/user/" + id, User.class);
+     * }
+     */
+
     @GetMapping
     @ResponseBody
-    public User queryUserById(@RequestParam("id") Long id) {
+    @HystrixCommand(fallbackMethod = "queryUserByIdFallback") //调用失败触发熔断机制
+    public String queryUserById1(@RequestParam("id") Long id) {
         //从Eureka获取服务实例
 //        List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
 //        ServiceInstance instance = instances.get(0);
@@ -45,7 +72,14 @@ public class UserController {
 //        return this.restTemplate.getForObject("http://" + instance.getHost() + ":" + instance.getPort() + "/user/" + id, User.class);
 
 //        使用ribbon负载均衡的写法
-        return this.restTemplate.getForObject("http://service-provider/user/" + id, User.class);
+        return this.restTemplate.getForObject("http://service-provider/user/" + id, String.class);
 
     }
+
+    //    这里面的"Long id"不能删除   @HystrixCommand(fallbackMethod = "")要求参数是一致的
+    public String queryUserByIdFallback(Long id) {
+        return "服务器正忙，请稍后再试";
+    }
+
+
 }
