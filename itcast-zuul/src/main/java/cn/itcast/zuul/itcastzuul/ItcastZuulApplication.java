19e96906fa728f99846690c80113e0cbed636b41
cococolabear <794055465@qq.com>
2021-02-04 17:10:08 +0800

    启动Zuul

---
+++ itcast-zuul/src/main/java/cn/itcast/zuul/itcastzuul/ItcastZuulApplication.java
@@ -0,0 +1,15 @@
+package cn.itcast.zuul.itcastzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy//启用Zuul组件
+public class ItcastZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItcastZuulApplication.class, args);
+	}
+
+}
---
+++ itcast-zuul/src/test/java/cn/itcast/zuul/itcastzuul/ItcastZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.itcast.zuul.itcastzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItcastZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
