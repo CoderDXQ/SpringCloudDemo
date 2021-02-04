ea06b2120ce2f673991b6146b793daccbd01bd98
cococolabear <794055465@qq.com>
2021-02-04 17:38:27 +0800

    Zuul四种路由配置

--- itcast-zuul/src/main/java/cn/itcast/zuul/itcastzuul/ItcastZuulApplication.java
+++ itcast-zuul/src/main/java/cn/itcast/zuul/itcastzuul/ItcastZuulApplication.java
@@ -2,10 +2,13 @@ package cn.itcast.zuul.itcastzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableZuulProxy//启用Zuul组件
+@EnableDiscoveryClient//启动Eureka发现
 public class ItcastZuulApplication {
 
 	public static void main(String[] args) {
