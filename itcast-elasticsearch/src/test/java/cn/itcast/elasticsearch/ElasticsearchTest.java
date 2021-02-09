1d667c25115b9bca9567831017e48e446b072de8
cococolabear <794055465@qq.com>
2021-02-09 21:42:49 +0800

    elasticsearch的各种增删查改及自定义查询

--- itcast-elasticsearch/src/test/java/cn/itcast/elasticsearch/ElasticsearchTest.java
+++ itcast-elasticsearch/src/test/java/cn/itcast/elasticsearch/ElasticsearchTest.java
@@ -1,12 +1,20 @@
 package cn.itcast.elasticsearch;
 
 import cn.itcast.elasticsearch.pojo.Item;
+import com.sun.jna.Native;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -81,6 +89,42 @@ public class ElasticsearchTest {
     public void testfindbetween() {
         List<Item> list = this.itemRepository.findByPriceBetween(3000d, 4000d);
         list.forEach(System.out::println);
+
+    }
+
+    @Test
+    public void testsearch() {
+
+//        通过查询构建器工具构建查询条件   查询标题中带"手机"字样的记录
+        MatchQueryBuilder queryBuilder = QueryBuilders.matchQuery("title", "手机");
+
+//        执行查询，获得结果集
+        Iterable<Item> items = this.itemRepository.search(queryBuilder);
+        items.forEach(System.out::println);
+    }
+
+    //    自定义查询
+    @Test
+    public void testNative() {
+
+//        构建自定义查询器
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+
+//        添加基本的查询条件
+        queryBuilder.withQuery(QueryBuilders.matchQuery("category", "手机"));
+
+//        添加分页条件，页码是从0开始
+        queryBuilder.withPageable(PageRequest.of(1, 2));
+
+//        添加排序条件，默认是降序
+        queryBuilder.withSort(SortBuilders.fieldSort("price").order(SortOrder.DESC));
+
+//        执行查询获取分页结果集
+        Page<Item> itemPage = this.itemRepository.search(queryBuilder.build());
+        System.out.println(itemPage.getTotalPages());
+        System.out.println(itemPage.getTotalElements());
+//        itemPage.forEach(System.out::println);
+        itemPage.getContent().forEach(System.out::println);
 
     }
 
